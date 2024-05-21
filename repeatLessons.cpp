a3b03bef5a84b740671cb67c2ca301607a6ea572
Davit <torgomyandavid96@gmail.com>
2024-05-21 22:13:13 +0400

    c++ lessons 2 cicle

--- repeatLessons.cpp
+++ repeatLessons.cpp
@@ -2,17 +2,35 @@
 using namespace std;
 
 
-void RepeatLessons() {
-    cout << "=== Start Lessons ===" << endl;
 
-    int age = 7;
+void STD_CIN_EXAMPLE() {
+
+    int count;
+    std::cin >> count;
 
 
-    {
-        {
-            int age = 5;
-        }
+    cout << "=== std::cin >> count === " << count << "\n";
+}
+
+
+void IF_AND_ELSE_EXAMPLE() {
+    int count;
+    std::cin >> count;
+
+    if(count == 10) {
+        cout << "count is equal => " << count << "\n";
+    } else if(count > 10) {
+        cout << "count is more than " << count << "\n";
+    } else {
+        cout << "count is less than " << count << "\n";
     }
 
-    std::cout << age << "\n";
+
+}
+
+
+void RepeatLessons() {
+    cout << "=== Start Lessons 2 ===" << endl;
+    // STD_CIN_EXAMPLE();
+    IF_AND_ELSE_EXAMPLE();
 }
